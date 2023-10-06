7946e4188ac9fc1a23bd48cc7fec224f6e58c36c
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-10-06 03:41:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+        return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name:"Steve",
+            email:"steve@steve.com",
+        }), 
+    })
+    .then(response => response.json())
+    .then(object => {
+        document.body.innerHTML += `<p>${object.id}</p>`
+    })
+    .catch(function(error){
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    })
+
+}
+submitData()
